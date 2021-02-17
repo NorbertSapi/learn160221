d67ffd432e4319f0f89086821281c468484f3fa2
Norbert Sapi <ncs.sapi@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email ncs.sapi@gmail.com>
2021-02-17 21:13:55 +0000

    This is an exercise to practice for loop. Create a method to return the sum of the Odd numbers in a range of numbers.

---
+++ src/com/gmail/ncs/sapi/Main.java
@@ -0,0 +1,20 @@
+package com.gmail.ncs.sapi;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// This is an exercise to practice for loop.
+        System.out.println(SumOddRange.isOdd(-1));
+    }
+}
+
+class SumOddRange {
+    public static boolean isOdd(int number) {
+        if (number > 0) {
+            return true;
+        }
+        {
+            return false;
+        }
+    }
+}
