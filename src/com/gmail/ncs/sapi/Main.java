eec736ebab792f1a403876757bb87698f5ead527
Norbert Sapi <ncs.sapi@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email ncs.sapi@gmail.com>
2021-02-18 08:05:17 +0000

    return odd number from the list

--- src/com/gmail/ncs/sapi/Main.java
+++ src/com/gmail/ncs/sapi/Main.java
@@ -4,13 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 	// This is an exercise to practice for loop.
-        System.out.println(SumOddRange.isOdd(-1));
+        System.out.println(SumOddRange.sumOdd(2, 7));
     }
 }
 
 class SumOddRange {
     public static boolean isOdd(int number) {
-        // return false if the number < 0
-        return number > 0;
+        // return false if the number is even
+        return (number % 2 == 1);
+    }
+
+    public static int sumOdd(int start, int end) {
+        for (; start < end; start++) {
+            if (isOdd(start)) {             // return odd numbers from the list
+                System.out.println(start);
+            }
+        }
+        return start;
     }
 }
