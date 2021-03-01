b0175667b1ee273cca2109a1c832ab538657b034
Norbert Sapi <ncs.sapi@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email ncs.sapi@gmail.com>
2021-03-01 22:04:35 +0000

    collect user input

--- src/com/gmail/ncs/sapi/Main.java
+++ src/com/gmail/ncs/sapi/Main.java
@@ -21,6 +21,7 @@ public class Main {
         System.out.println(SumOddRange.sumOdd(13, 13));
         System.out.println(SumOddRange.sumOdd(100, -100));
         System.out.println(SumOddRange.sumOdd(100, 1000));
+
         scanner.close();
     }
 }
